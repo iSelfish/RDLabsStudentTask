8e666500f6f7ff113e75cb1916be0ad03b59a716
Dmytro_Kolos <zoraks9744@gmail.com>
2020-02-28 13:45:13 +0200

    checking jenkins trigger

--- src/test/java/stepDefs/runners/NewTaskRunner.java
+++ src/test/java/stepDefs/runners/NewTaskRunner.java
@@ -1,7 +1,7 @@
 package stepDefs.runners;
 
 import net.serenitybdd.jbehave.annotations.Metafilter;
-
+//commit test
 @Metafilter("+newTask")
 public class NewTaskRunner extends BaseRunnerConfiguration {
 }
